70e2f2859a20148ae5cc74e1c7b97bd93270e604
killiyun <killian.clarke@protonmail.com>
2021-11-13 19:04:44 +0000

    current native react html render does not work :)

---
+++ app.tsx
@@ -0,0 +1,72 @@
+import express from "express";
+import React from 'react';
+import { useWindowDimensions } from 'react-native';
+import RenderHtml from 'react-native-render-html';
+import { Client } from "@googlemaps/google-maps-services-js";
+import { config } from "dotenv";
+
+config();
+
+export const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.status(200).send('Hello world!').end();
+});
+
+let map: google.maps.Map;
+function initMap(): void {
+    map = new google.maps.Map(document.getElementById("Map") as HTMLElement, {
+        center: { lat: 50.937349, lng: -1.397909},
+        zoom: 8,
+    });
+}
+
+// Start the server
+
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log("Press Ctrl+C to quit.");
+});
+// [END gae_node_request_example]
+
+const client = new Client({});
+
+client
+  .elevation({
+    params: {
+      locations: [{ lat: 50.937349, lng: -1.397909 }],
+      key: process.env.GOOGLE_MAPS_API_KEY || "",
+    },
+    timeout: 1000, // milliseconds
+  })
+  .then((r) => {
+    console.log(r.data.results[0].elevation);
+  })
+  .catch((e) => {
+    console.log(e.response.data.error_message);
+  });
+
+/*
+const source = {
+    html: `<iframe width="600" height="450" style="border:0" loading="lazy" allowfullscreen src="https://www.google.com/maps/embed/v1/place?q=place_id:ChIJKeyH6vVzdEgRVp9Uu4KZl1M&key=AIzaSyBmoJGxjYSO1MToeKhfM9Cz6-dMB4i2-Tg"></iframe>`
+};
+*/
+
+
+const source = {
+    html: `
+<p style='text-align:center;'>
+  Hello Woarld!
+</p>`
+};
+
+export default function App() {
+    const { width } = useWindowDimensions();
+    return (
+        <RenderHtml
+            contentWidth={width}
+            source={source}
+        />
+    );
+}
